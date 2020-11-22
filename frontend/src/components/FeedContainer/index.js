2f3a8d0fa708354450fa2a2333eec61cc25db2a1
Jacob Grooms <grooms.jacob@gmail.com>
2020-11-22 14:10:53 -0500

    feed styling, taking a break

--- frontend/src/components/FeedContainer/index.js
+++ frontend/src/components/FeedContainer/index.js
@@ -2,28 +2,27 @@ import React, {useEffect } from 'react';
 import * as photoActions from "../../store/photos";
 import {useDispatch, useSelector} from "react-redux";
 import Photo from '../Photo'
+import './FeedContainer.css'
 
 const FeedContainer = () => {
   // Subscribe to store
   const statePhotos = useSelector((state) => state.photo);
   const dispatch = useDispatch();
-  // const [photos, setPhotos] = useState('');
 
   // will run on intial mount and any subsequent dispatch actions.
   useEffect(() => {
-    console.log(statePhotos)
-    console.log("dispatch")
+    // console.log("dispatch")
+
     // dispatches custom action
     return dispatch(photoActions.getAllPhotos()).catch((res) => console.log("ERROR"));
 
   }, [dispatch])
 
-
+  // statePhoto.photos is needed, as that is where the null value lives
   return ( statePhotos.photos &&
-		(<div>
-			{console.log(statePhotos.photos)}
+		(<div className="feed-container">
 			{statePhotos.photos.map((photo) => (
-				<Photo photo={photo} />
+				<div className="feed-item"><Photo photo={photo} /></div>
 			))}
 		</div>)
 	);
--- frontend/src/components/Photo/index.js
+++ frontend/src/components/Photo/index.js
@@ -3,10 +3,6 @@ import React, { useEffect } from 'react';
 import "./Photo.css"
 
 const Photo = (props) => {
-  // const statePhotos = useSelector((state) => state.photos);
-  // useEffect(() => {
-  //   //TODO rerender when photos update
-  // }, [statePhotos])
 
   return (
     <img className="feed-photo" src={`https://s3.us-east-2.amazonaws.com/the-pass/${props.photo}`} alt="feed" />
