6e89919361f99b317f20c62ca6a44b2cf96650db
Jacob Grooms <grooms.jacob@gmail.com>
2020-11-23 16:05:51 -0500

    styling feature page

--- frontend/src/components/DisplayPhoto/index.js
+++ frontend/src/components/DisplayPhoto/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import * as photoActions from "../../store/photos";
+import Photo from "../Photo";
 import "./displayPhoto.css"
 
 const DisplayPhoto = () => {
@@ -17,14 +18,24 @@ const DisplayPhoto = () => {
 
   return ( statePhotos &&
 		 (
+       <>
        <div className="display-container">
          <img
            className="display-photo"
            src={`https://s3.us-east-2.amazonaws.com/the-pass/${name}`}
            alt="feed"
          />
-
        </div>
+        <p className="other-photos-header">More by this user:</p>
+        <div className="preview-container">
+          {statePhotos.photos.map(photo => (
+            <div className="preview-photo">
+              <Photo photo={photo.filename} />
+
+            </div>
+            ))}
+        </div>
+       </>
 		)
 	);
 }
